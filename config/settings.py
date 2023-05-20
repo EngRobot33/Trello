b0910b648920430491fe00edb051cac0897e365c
EngRobot33 <hmdkhsrvee@gmail.com>
2023-05-20 12:16:09 +0330

    fix: docker-compose

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
